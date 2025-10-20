8fd81f1cf8241d91ee2755bf8968bbdd8c1faf86
Qian, Xusheng <xuq8@pitt.edu>
2025-10-20 13:34:19 -0400

    feat: restructure project and set constants

---
+++ my-app/src/constants/index.ts
@@ -0,0 +1,11 @@
+export const API_BASE_URL = 'http://localhost:5000';
+
+export const PAGINATION = {
+  DEFAULT_PAGE_SIZE: 10,
+  DEFAULT_PAGE: 1,
+} as const;
+
+export const ROUTES = {
+  HOME: '/',
+  SEARCH: '/search',
+} as const;

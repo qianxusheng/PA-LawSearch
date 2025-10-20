60e74a9ee77c2417994da6cbf99ec51a6061ce0f
Qian, Xusheng <xuq8@pitt.edu>
2025-10-20 13:49:52 -0400

    fix: add unified export

---
+++ my-app/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as SearchBar } from './SearchBar';
+export { default as CaseDetail } from './CaseDetail';
+export { default as SearchResults } from './SearchResults';
+export { default as ResultCard } from './ResultCard';
